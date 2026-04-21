0d713ce0dbdd5222e3ce6f3578133dabab37b01f
aryasakshi13 <sakshiarya884@gmail.com>
2026-04-21 17:41:29 +0530

    Handle api call

--- src/app.js
+++ src/app.js
@@ -1,15 +1,30 @@
   const express = require('express');
+   const app = express();
+ 
 
-  const app = express();
-  
- app.use("/test", (req,res) =>{
-    res.send("hello server")
+   app.use("/user", (req, res)=>{
+    res.send("helloooooooo");
  })
+  
+//   This will only handle et call to user  
+ app.get("/user", (req,res) =>{
+    res.send({fistName:"Sakshi", lastName:"Arya"})
+ })
+  
+  app.post("/user", (req, res)=>{
+    // firstly write the logic of Saving data to db  
+       res.send("Successfully save the data to db");
+    
+  })
 
- app.use("/hello", (req, res)=>{
+   app.delete("/user", (req, res) =>{
+     res.send("deleted user Successfully");
+   })
+   // This will match all the http method API calls to /test
+     app.use("/hello", (req, res)=>{
     res.send("hello from hello path");
  })
- 
+  
    
   app.listen(7777, ()=> {
      console.log("Server is Successfully listening on port 7777");
