0b3a8675470051df263d4fc59e4c864f6e78de8d
aryasakshi13 <sakshiarya884@gmail.com>
2026-04-29 14:54:29 +0530

    Added custom validation of Gender in User Schema

--- src/models/User.js
+++ src/models/User.js
@@ -2,24 +2,47 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
    firstName: {
-    type: String
+    type: String,
+    minLength: 4,
+    maxLength: 50,
+    required: true 
+
    },
     lastName: {
       type: String
+
    },
    emailId: {
-      type: String
+      type: String,
+      required: true,
+      unique: true 
    },
    password: {
-    type: String
+    type: String,
+    minLength: 8 ,
+    maxLength: 20,
+    required: true
    },
     age: {
         type: String
     },
     gender: {
-        type: String
-    }
-})
+        type: String,
+        validate(value){
+            if(!["male", "Female", "Other"].includes(value)){
+                throw new Error("Gender data is not valid");
+            }
+        },
+       
+    },
+    about: {
+        default: "We are motivated techie!",
+    },
+    skills:{
+         type: [string],
+    },
+
+});
 
 const User = mongoose.model("User", userSchema)
     module.exports = User ;
